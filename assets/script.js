260008822def639eaba5e3edfbb5620a537f514a
t.bretones <titouan.bretones@gmail.com>
2023-04-27 11:32:59 +0200

    flèche

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,22 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+/*
+	Les variables qui sont composées de plusieurs mots peuvent être notés en :
+		- Snake case _ : majuscule_minuscule
+		- Camel case > majusculeMinuscule - Première lettre du premier mot toujours en minuscule
+
+	Pour les sélecteurs CSS, utilises plutôt querySelector au lieu de getElementById
+*/
+
+const leftArrow = document.querySelector('.arrow_left');
+const rightArrow = document.querySelector('.arrow_right');
+
+leftArrow.addEventListener('click', () => {
+  console.log('La flèche gauche a été cliquée!');
+});
+
+rightArrow.addEventListener('click', () => {
+  console.log('La flèche droite a été cliquée!');
+});
